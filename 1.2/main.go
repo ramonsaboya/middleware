1311afaa5526072123a1e49308603b29dbc47cff
Ramon Saboya <ramonsaboyag@gmail.com>
2020-08-29 18:27:47 -0300

    fix exercise 2

--- 1.2/main.go
+++ 1.2/main.go
@@ -7,55 +7,73 @@ import (
 	"time"
 )
 
-var buffer = -1
-var cond = sync.NewCond(&sync.Mutex{})
-var cond2 = sync.NewCond(&sync.Mutex{})
-var wg sync.WaitGroup
-
-func consume(id int) int {
-	cond2.Signal()
-
-	cond.L.Lock()
-	wg.Add(1)
-	for buffer == -1 {
-		cond.Wait()
-	}
-	value := buffer
-	wg.Done()
-	cond.L.Unlock()
-	return value
+type consumerRes struct {
+	id   int
+	x    *int
+	cond *sync.Cond
 }
 
-func produce(n int) {
-	cond.L.Lock()
-	buffer = n
-	fmt.Printf("--- broadcasted %d\n", n)
-	cond.L.Unlock()
-	cond.Broadcast()
-	wg.Wait()
-	cond.L.Lock()
-	buffer = -1
-	wg = sync.WaitGroup{}
-	cond.L.Unlock()
+var exit = make(chan bool)
+
+var consQueueCond = sync.NewCond(&sync.Mutex{})
+var consQueue = make([]*consumerRes, 0)
+
+func freeConsumers(n int) {
+	consQueueCond.L.Lock()
+	for len(consQueue) == 0 {
+		consQueueCond.Wait()
+	}
+	for _, cons := range consQueue {
+		cons.cond.L.Lock()
+		cons.x = &n
+		fmt.Printf("producer signaled for consumer %d\n", cons.id)
+		cons.cond.Signal()
+		cons.cond.L.Unlock()
+	}
+	consQueue = make([]*consumerRes, 0)
+	consQueueCond.L.Unlock()
+}
+
+func consumerThread(id int) {
+	for {
+		cond := sync.NewCond(&sync.Mutex{})
+		res := consumerRes{id, nil, cond}
+
+		consQueueCond.L.Lock()
+		consQueue = append(consQueue, &res)
+		consQueueCond.Signal()
+		consQueueCond.L.Unlock()
+
+		cond.L.Lock()
+		for res.x == nil {
+			cond.Wait()
+		}
+		cond.L.Unlock()
+
+		fmt.Printf("%d consumer, consumed: %d\n", id, *res.x)
+	}
+}
+
+func producerThread() {
+	// time.Sleep(2 * time.Second)
+	// for i := 0; i < 10; i++ {
+	for {
+		x := rand.Intn(100)
+		fmt.Printf("produced %d\n", x)
+
+		freeConsumers(x)
+	}
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	consumers := 2
+	consumers := 1 + rand.Intn(10)
 	for i := 0; i < consumers; i++ {
-		go func(id int) {
-			for {
-				fmt.Printf("Consumed: %d\n", consume(id))
-			}
-		}(i)
+		go consumerThread(i + 1)
 	}
 
-	for {
-		produce(rand.Intn(100))
+	go producerThread()
 
-		cond2.L.Lock()
-		cond2.Wait()
-		cond2.L.Unlock()
-	}
+	<-exit
 }
